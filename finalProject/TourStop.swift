435fccd6fc20fbd7267339136396aec0202eabca
isabellahoch <ihochschild@gmail.com>
2019-07-17 15:27:25 -0700

    san franSICKO MODE

---
+++ finalProject/TourStop.swift
@@ -0,0 +1,15 @@
+//
+//  TourStop.swift
+//  finalProject
+//
+//  Created by Apple on 7/17/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class TourStop {
+    var name = ""
+    var details = ""
+    var address = ""
+}
